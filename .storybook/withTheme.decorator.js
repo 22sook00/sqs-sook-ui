bc7690e609f363c0c6b7f5a532cc620235ead088
Lee Sookyoung <80618616+22sook00@users.noreply.github.com>
2023-05-25 21:43:53 +0900

    Set storybook with emotion

---
+++ .storybook/withTheme.decorator.js
@@ -0,0 +1,35 @@
+import { css, Global, ThemeProvider, useTheme } from '@emotion/react';
+import { lightTheme, darkTheme } from '../src/theme';
+
+const THEMES = {
+  light: lightTheme,
+  dark: darkTheme,
+};
+
+// Sets the background based on theme
+const GlobalStyles = () => {
+  const theme = useTheme();
+
+  return (
+    <Global
+      styles={css`
+        html,
+        body {
+          background-color: ${theme.colors.background};
+          color: ${theme.colors.text};
+        }
+      `}
+    />
+  );
+};
+
+export const withTheme = (Story, context) => {
+  const { theme } = context.globals;
+
+  return (
+    <ThemeProvider theme={THEMES[theme] || THEMES['light']}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  );
+};
---
+++ src/stories/Button2.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const button2 = () => {
+  return <div>button2</div>;
+};
+
+export default button2;
